51b1ff7ed2a8a45c257e14a5b68d78e8f30bfc8c
FilonIhor <107698542+FilonIhor@users.noreply.github.com>
2022-11-02 20:42:02 +0200

    delete

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const itemsEL = document.querySelectorAll('.item');
-console.log(`Number of categories: ${itemsEL.length}`);
-console.log('');
-itemsEL.forEach(item => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  const listEL = item.lastElementChild;
-  console.log(`Elements: ${listEL.children.length}`);
-  console.log('');
-});
